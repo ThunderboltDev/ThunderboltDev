c072b47adc42fed2ba67d90fb30bc5519a8512f3
Programmer80101 <programmer80101@gmail.com>
2026-02-01 17:44:50 +0530

    fix sitemap

--- app/blog/[category]/sitemap.xml/route.ts
+++ app/blog/[category]/sitemap.xml/route.ts
@@ -15,19 +15,20 @@ export async function GET(
   const posts = await getAllPosts();
   const categoryPosts = posts.filter((post) => post.category === category);
 
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+  const xml = `
+    <?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${categoryPosts
-      .map(
-        (post) => `
-      <url>
-        <loc>${url}/blog/${post.category}/${post.slug}</loc>
-        <lastmod>${post.data.lastModified}</lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>0.6</priority>
-      </url>`,
-      )
-      .join("")}
+      ${categoryPosts
+        .map(
+          (post) => `
+        <url>
+          <loc>${url}/blog/${post.category}/${post.slug}</loc>
+          <lastmod>${(post.data.lastModified ?? post.data.date).toISOString()}</lastmod>
+          <changefreq>weekly</changefreq>
+          <priority>0.6</priority>
+        </url>`,
+        )
+        .join("")}
     </urlset>`;
 
   return new Response(xml, {
